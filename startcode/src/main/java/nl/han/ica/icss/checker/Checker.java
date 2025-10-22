3884ba238c7313a2e94e6776cca87ca738163642
nielsvaneck <vaneckniels@gmail.com>
2025-10-22 14:56:53 +0200

    checker op width en color

--- startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -1,7 +1,7 @@
 package nl.han.ica.icss.checker;
 
 import nl.han.ica.icss.ast.*;
-import nl.han.ica.icss.ast.literals.ColorLiteral;
+import nl.han.ica.icss.ast.literals.*;
 import nl.han.ica.icss.ast.types.ExpressionType;
 
 import java.util.HashMap;
@@ -34,6 +34,26 @@ public class Checker {
         if (declaration.property.name.equals("width")){
             if (declaration.expression instanceof ColorLiteral){
                 declaration.setError("Property 'width' can't be a color");
+            }
+            if (declaration.expression instanceof ScalarLiteral){
+                declaration.setError("Property 'width' must be px or %");
+            }
+            if (declaration.expression instanceof BoolLiteral){
+                declaration.setError("Property 'width' can't be boolean");
+            }
+        }
+        if (declaration.property.name.contains("color")){
+            if (declaration.expression instanceof PixelLiteral){
+                declaration.setError("Property '" + ((PixelLiteral) declaration.expression).value + "' must be a color");
+            }
+            if (declaration.expression instanceof ScalarLiteral){
+                declaration.setError("Property '" + ((ScalarLiteral) declaration.expression).value + "' must be a color");
+            }
+            if (declaration.expression instanceof PercentageLiteral){
+                declaration.setError("Property '" + ((PercentageLiteral) declaration.expression).value + "' must be a color");
+            }
+            if (declaration.expression instanceof BoolLiteral){
+                declaration.setError("Property '" + ((BoolLiteral) declaration.expression).value + "' must be a color");
             }
         }
     }
